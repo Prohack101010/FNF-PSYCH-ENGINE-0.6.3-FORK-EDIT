3e96ffe558ac0d5689873c44417453ed6e377ed5
SrNikolasGitPort <156230428+SrNikolasGitPort@users.noreply.github.com>
2024-06-10 18:32:32 -0300

    FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -1,5 +1,4 @@
 package openfl.display;
-
 import haxe.Timer;
 import openfl.events.Event;
 import openfl.text.TextField;
@@ -12,11 +11,9 @@ import openfl.display._internal.stats.DrawCallContext;
 #if flash
 import openfl.Lib;
 #end
-
 #if openfl
 import openfl.system.System;
 #end
-
 /**
 	The FPS class provides an easy-to-use monitor to display
 	the current frame rate of an OpenFL project
@@ -31,18 +28,14 @@ class FPS extends TextField
 		The current frame rate, expressed using frames-per-second
 	**/
 	public var currentFPS(default, null):Int;
-
 	@:noCompletion private var cacheCount:Int;
 	@:noCompletion private var currentTime:Float;
 	@:noCompletion private var times:Array<Float>;
-
 	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000)
 	{
 		super();
-
 		this.x = x;
 		this.y = y;
-
 		currentFPS = 0;
 		selectable = false;
 		mouseEnabled = false;
@@ -50,11 +43,9 @@ class FPS extends TextField
 		autoSize = LEFT;
 		multiline = true;
 		text = "FPS: ";
-
 		cacheCount = 0;
 		currentTime = 0;
 		times = [];
-
 		#if flash
 		addEventListener(Event.ENTER_FRAME, function(e)
 		{
@@ -63,55 +54,45 @@ class FPS extends TextField
 		});
 		#end
 	}
-
 	// Event Handlers
 	@:noCompletion
 	private #if !flash override #end function __enterFrame(deltaTime:Float):Void
 	{
 		currentTime += deltaTime;
 		times.push(currentTime);
-
 		while (times[0] < currentTime - 1000)
 		{
 			times.shift();
 		}
-
 		var currentCount = times.length;
 		currentFPS = Math.round((currentCount + cacheCount) / 2);
 		if (currentFPS > ClientPrefs.framerate) currentFPS = ClientPrefs.framerate;
-
 		if (currentCount != cacheCount /*&& visible*/)
 		{
 			text = "FPS: " + currentFPS;
 			var memoryMegas:Float = 0;
-			
 			#if openfl
 			memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
 			text += "\nMemory: " + memoryMegas + " MB";
 			#end
-
 			text += "\nExtra Keys v" + MainMenuState.extraKeysVersion;
 			#if CHECK_FOR_UPDATES
 			if (MainMenuState.extraKeysVersion != TitleState.extraKeyUpdateVersion) {
 				text += "\nUpdate available!";
 			}
 			#end
-
 			textColor = 0xFFFFFFFF;
 			if (memoryMegas > 3000 || currentFPS <= ClientPrefs.framerate / 2)
 			{
 				textColor = 0xFFFF0000;
 			}
-
 			#if (gl_stats && !disable_cffi && (!html5 || !canvas))
 			text += "\ntotalDC: " + Context3DStats.totalDrawCalls();
 			text += "\nstageDC: " + Context3DStats.contextDrawCalls(DrawCallContext.STAGE);
 			text += "\nstage3DDC: " + Context3DStats.contextDrawCalls(DrawCallContext.STAGE3D);
 			#end
-
 			text += "\n";
 		}
-
 		cacheCount = currentCount;
 	}
 }
